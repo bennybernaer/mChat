d62f7887a03be2922566584d33665f4641e94215
BianCionaa <michelheusschen@gmail.com>
2016-02-16 21:48:22 +0100

    Update voor versie 1.0.0-RC1

--- language/nl/info_acp_mchat.php
+++ language/nl/info_acp_mchat.php
@@ -5,16 +5,19 @@
 * @copyright (c) 2015 dmzx - http://www.dmzx-web.net
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 * Dutch translation by Dutch Translators (https://github.com/dutch-translators)
+
 *
 */
 if (!defined('IN_PHPBB'))
 {
 	exit;
 }
+
 if (empty($lang) || !is_array($lang))
 {
 	$lang = array();
 }
+
 // DEVELOPERS PLEASE NOTE
 //
 // All language files should use UTF-8 as their encoding and the files must not contain a BOM.
@@ -29,43 +32,45 @@ if (empty($lang) || !is_array($lang))
 //
 // Some characters for use
 // ’ » “ ” …
+
 $lang = array_merge($lang, array(
-	// UMIL stuff
-	'ACP_MCHAT_CONFIG'				=> 'Configuratie',
-	'ACP_CAT_MCHAT'					=> 'mChat',
-	'ACP_MCHAT_TITLE'				=> 'Mini-Chat',
-	'ACP_MCHAT_TITLE_EXPLAIN'		=> 'Een mini chat (aka “shout box”) voor gebruik op jouw forum',
-	'MCHAT_TABLE_DELETED'			=> 'De mChat tabel is succesvol verwijderd',
-	'MCHAT_TABLE_CREATED'			=> 'De mChat tabel is succesvol aangemaakt',
-	'MCHAT_TABLE_UPDATED'			=> 'De mChat tabel is succesvol bijgewerkt.',
-	'MCHAT_NOTHING_TO_UPDATE'		=> 'Niks te doen..... doorgaan',
-	'UCP_CAT_MCHAT'					=> 'mChat voorkeuren',
-	'UCP_MCHAT_CONFIG'				=> 'Gebruiker mChat voorkeuren',
+	'ACP_MCHAT_CONFIG'						=> 'Configuratie',
+	'ACP_CAT_MCHAT'							=> 'mChat',
+	'ACP_MCHAT_TITLE'						=> 'Mini-Chat Extensie voor phpBB 3.1',
+	'ACP_MCHAT_VERSION'						=> 'Versie',
+	'UCP_CAT_MCHAT'							=> 'mChat Voorkeuren',
+	'UCP_MCHAT_CONFIG'						=> 'Gebruikers mChat Voorkeuren',
+	
+	// ACP configuration sections
+	'MCHAT_SETTINGS_INDEX'					=> 'Forumoverzicht instellingen',
+	'MCHAT_SETTINGS_CUSTOM'					=> 'Aangepaste pagina instellingen',
+	'MCHAT_SETTINGS_ARCHIVE'				=> 'Archiefpagina instellingen',
+	'MCHAT_SETTINGS_POSTS'					=> 'Nieuwe berichten instellingen',
+	'MCHAT_SETTINGS_MESSAGES'				=> 'Berichten instellingen',
+	'MCHAT_SETTINGS_PRUNE'					=> 'Opruim instellingen',
+	'MCHAT_SETTINGS_STATS'					=> 'Wie is er aan het chatten instellingen',
+
 	// ACP entries
 	'ACP_MCHAT_RULES'				=> 'Regels',
-	'ACP_MCHAT_RULES_EXPLAIN'		=> 'Verander hier de regels van het forum.  Elke regel op een nieuwe lijn.<br />Je kunt maximaal 255 karakters gebruiken.<br /><strong>Deze boodschap kan worden vertaald..</strong> (Je moet de mchat_lang.php file aanpassen en lees de instructies).',
-	'LOG_MCHAT_CONFIG_UPDATE'		=> '<strong>Update mChat configuratie </strong>',
+	'ACP_MCHAT_RULES_EXPLAIN'		=> 'Voer de regels van het forum hier in. Elke regel op een nieuwe lijn. HTML code is toegestaan <em>Er is een limiet van 255 tekens.</em><br />Dit bericht kan worden vertaald: bewerk de MCHAT_RULES_MESSAGE taal code in /ext/dmzx/mchat/language/XX/common.php.',
+	'LOG_MCHAT_CONFIG_UPDATE'		=> '<strong>Update mChat configuratie</strong>',
 	'MCHAT_CONFIG_SAVED'			=> 'Mini Chat configuratie is bijgewerkt',
 	'MCHAT_TITLE'					=> 'Mini-Chat',
-	'MCHAT_VERSION'					=> 'Versie:',
-	'MCHAT_ENABLE'					=> 'mChat extensie inschakelen',
-	'MCHAT_ENABLE_EXPLAIN'			=> 'In- of uitschakelen van deze extensie.',
 	'MCHAT_AVATARS'					=> 'Toon avatars',
-	'MCHAT_AVATARS_EXPLAIN'			=> 'Als je ja hebt aangevinkt, verkleinde gebruikers avatars zullen worden getoond',
-	'MCHAT_ON_INDEX'				=> 'mChat tonen oOp de indexpagina',
-	'MCHAT_ON_INDEX_EXPLAIN'		=> 'Toestaan om de mChat te tonen op de indexpagina.',
+	'MCHAT_AVATARS_EXPLAIN'			=> 'Als je ja hebt aangevinkt, zullen verkleinde gebruikers avatars worden getoond',
+	'MCHAT_ON_INDEX'				=> 'mChat tonen op de indexpagina',
 	'MCHAT_INDEX_HEIGHT'			=> 'Indexpagina hoogte',
 	'MCHAT_INDEX_HEIGHT_EXPLAIN'	=> 'De hoogte van de mChat box op de indexpagina uitgedrukt in pixels, kun je hier aanpassen.<br /><em>Je bent gelimiteerd tussen de 50 en 1000</em>.',
 	'MCHAT_LOCATION'				=> 'Locatie op het Forum',
-	'MCHAT_LOCATION_EXPLAIN'		=> 'Kies de locatie van de mChat op de indexpagina.',
 	'MCHAT_TOP_OF_FORUM'			=> 'Bovenaan op het Forum',
 	'MCHAT_BOTTOM_OF_FORUM'			=> 'Onderaan op het Forum',
-	'MCHAT_REFRESH'					=> 'Vernieuwen',
+	'MCHAT_REFRESH'					=> 'Vernieuwen'
 	'MCHAT_REFRESH_EXPLAIN'			=> 'Aantal seconden dat de mChat automatische ververst wordt.<br /><em>Je bent gelimiteerd tussen 5 en 60 seconden</em>.',
+	'MCHAT_LIVE_UPDATES'					=> 'Live updates van bewerkte of verwijderde berichten',
+	'MCHAT_LIVE_UPDATES_EXPLAIN'			=> 'Als een gebruiker een bericht bewerkt of verwijderd, zullen de veranderen meteen zichtbaar zijn voor anderen, zonder de pagina te hoeven herladen. Schakel dit uit als je laadproblemen ondervindt.',
 	'MCHAT_PRUNE'					=> 'Inschakelen opschonen van berichten',
 	'MCHAT_PRUNE_EXPLAIN'			=> 'Vink Ja aan als je het opschonen van berichten wilt inschakelen.<br /><em>Werkt alleen als een gebruiker de gemaakte of archief pagina bekijkt</em>.',
 	'MCHAT_PRUNE_NUM'				=> 'Het aantal berichten welke bewaard moeten worden in de mChat',
-	'MCHAT_PRUNE_NUM_EXPLAIN'		=> 'Geef hier het aantal in, van de berichten welke je bewaard wilt houden in de mChat.',
 	'MCHAT_MESSAGE_LIMIT'			=> 'Berichten limiet',
 	'MCHAT_MESSAGE_LIMIT_EXPLAIN'	=> 'Maximaal aantal berichten, welke getoond worden in de mChat.<br /><em>Aanbevolen is tussen de 10 en 30 berichten</em>.',
 	'MCHAT_MESSAGE_NUM'				=> 'Indexpagina berichten limiet',
@@ -74,6 +79,8 @@ $lang = array_merge($lang, array(
 	'MCHAT_ARCHIVE_LIMIT_EXPLAIN'	=> 'Het maximale aantal berichten, welke getoond worden in de mChat op de Archief pagina.<br /><em>Aanbevolen is tussen de 25 en 50 berichten</em>.',
 	'MCHAT_FLOOD_TIME'				=> 'Wachttijd plaatsen volgende bericht, na reeds geplaatst bericht',
 	'MCHAT_FLOOD_TIME_EXPLAIN'		=> 'Het aantal seconden dat een gebruiker moet wachten om een volgend bericht te plaatsen in de mChat.<br /><em>Aanbevolen is tussen de 5 en 30 seconden, 0 is uitschakelen van deze functie</em>.',
+	'MCHAT_EDIT_DELETE_LIMIT'				=> 'De tijd om berichten te bewerken of verwijderen',
+	'MCHAT_EDIT_DELETE_LIMIT_EXPLAIN'		=> 'Berichten ouder dan het aantal seconden kunnen niet meer door de gebruiker worden bewerkt.<br />Gebruikers die <em>bewerk/verwijder permissies of moderator permissies hebben zijn uitgesloten</em> van deze tijdlimiet.<br />Zet op 0 om het bewerken/verwijderen van berichten altijd toe te staan.',
 	'MCHAT_MAX_MESSAGE_LENGTH'		=> 'Maximale berichten lengte',
 	'MCHAT_MAX_MESSAGE_LENGTH_EXPLAIN'	=> 'Maximaal toegestane aantal karakters per gepost bericht.<br /><em>anbevolen is tussen de 100 en 500 karakters, 0 is uitschakelen van deze functie</em>.',
 	'MCHAT_CUSTOM_PAGE'				=> 'Aangepaste pagina',
@@ -83,11 +90,7 @@ $lang = array_merge($lang, array(
 	'MCHAT_DATE_FORMAT'				=> 'Datum weergave',
 	'MCHAT_DATE_FORMAT_EXPLAIN'		=> 'De gebruikte syntax is identiek aan de PHP <a href="http://www.php.net/date">date()</a> functie.',
 	'MCHAT_CUSTOM_DATEFORMAT'		=> 'Aangepast…',
-	'MCHAT_WHOIS'					=> 'Whois',
-	'MCHAT_WHOIS_EXPLAIN'			=> 'Toestaan om gebruikers te laten zien die gebruik maken van de mChat',
-	'MCHAT_WHOIS_REFRESH'			=> 'Whois vernieuwen',
-	'MCHAT_WHOIS_REFRESH_EXPLAIN'	=> 'Aantal seconden voordat whois statistieken worden vernieuwd.<br /><em>Je bent gelimiteerd tussen de 30 en 300 seconden</em>.',
-	'MCHAT_BBCODES_DISALLOWED'		=> 'Niet toegestane bbcodes',
+	'MCHAT_BBCODES_DISALLOWED'				=> 'Verboden bbcodes',
 	'MCHAT_BBCODES_DISALLOWED_EXPLAIN'	=> 'Hier kun je de bbcodes plaatsen, die <strong>niet</strong>zijn toegstaan<strong>not</strong> om te gebruiken in een bericht.<br />Aparte BBCodes met een verticale balk , bijvoorbeeld: <br />b|i|u|code|list|list=|flash|quote and/or a %scustom bbcode tag name%s',
 	'MCHAT_STATIC_MESSAGE'			=> 'Statisch bericht',
 	'MCHAT_STATIC_MESSAGE_EXPLAIN'	=> 'Hier kan je een statisch bericht definieren, welke getoond wordt aan de gebruikers van de mChat.<br />Stel 0 in om de vertoning uit te schakelen.  Je bent gelimiteerd tot 255 karakters.<br /><strong>Deze boodschap kan worden vertaald..</strong> (Je moet de mchat_lang.php file aanpassen en lees de instructies).',
@@ -97,54 +100,71 @@ $lang = array_merge($lang, array(
 	'MCHAT_OVERRIDE_SMILIE_LIMIT_EXPLAIN'	=> 'Stel hier het aantal smilie limiet in voor de chat berichten',
 	'MCHAT_OVERRIDE_MIN_POST_CHARS'	=> 'Minimum karakters limit',
 	'MCHAT_OVERRIDE_MIN_POST_CHARS_EXPLAIN'	=> 'Stel ja in om het aantal minimum karakters in te stellen voor een chat bericht',
-	'MCHAT_NEW_POSTS'				=> 'Toon nieuwe berichten',
-	'MCHAT_NEW_POSTS_EXPLAIN'		=> 'Stel ja in om nieuwe berichten te plaatsen in een chat bericht<br /><strong>Je moet de add-on voor de nieuwe post meldingen geïnstalleerd hebben</strong> (binnen de directory van de download voor deze extensie).',
-	'MCHAT_MAIN'					=> 'Hoofd Configuratie',
-	'MCHAT_STATS'					=> 'Wie is aan het chatten',
+	'MCHAT_NEW_POSTS_TOPIC'				=> 'Toon nieuwe berichten',
+	'MCHAT_NEW_POSTS_TOPIC_EXPLAIN'		=> 'Stel ja in om nieuwe berichten te plaatsen in een chat bericht<br /><strong>Je moet de add-on voor de nieuwe post meldingen geïnstalleerd hebben</strong> (binnen de directory van de download voor deze extensie).',
+	'MCHAT_NEW_POSTS_REPLY'					=> 'Toon nieuwe berichten',
+	'MCHAT_NEW_POSTS_REPLY_EXPLAIN'			=> 'Allow replies from the forum to be posted in the chat.',
+	'MCHAT_NEW_POSTS_EDIT'					=> 'Toon bewerkte berichten',
+	'MCHAT_NEW_POSTS_EDIT_EXPLAIN'			=> 'Sta toe dat bewerkte berichten uit het forum worden geplaatst in de chat.',
+	'MCHAT_NEW_POSTS_QUOTE'					=> 'Toon gequote berichten',
+	'MCHAT_NEW_POSTS_QUOTE_EXPLAIN'			=> 'Sta toe dat quotes uit berichten uit het forum worden geplaats in de chat.',
+
+	'MCHAT_WHOIS'							=> 'Toon <em>Wie is aan het chatten</em> onder de chat',
 	'MCHAT_STATS_INDEX'				=> 'Statistieken op de indexpagina tonen',
 	'MCHAT_STATS_INDEX_EXPLAIN'		=> 'Laat zien wie aan het chatten is in de statistieken sectie op het forum',
-	'MCHAT_MESSAGES'				=> 'Berichten instellingen',
-	'MCHAT_PAUSE_ON_INPUT'			=> 'Pauze op eventuele inactiviteit van mChat',
-	'MCHAT_PAUSE_ON_INPUT_EXPLAIN'	=> 'Stel je ja in, dan wordt mChat niet automatisch vernieuwd, todat een gebruiker een bericht plaatst in mChat',
-	// error reporting
-	'MCHAT_NEEDS_UPDATING'	=> 'De mChat extensie moet worden bijgewerkt. Neem contact op met de forum beheerder om de mChat bij te laten werken.',
-	'MCHAT_WRONG_VERSION'	=> 'De verkeerde versie van de extensie is geïnstalleerd.  Gebruik a.u.b. de %sinstaller%s van de nieuwe versie voor eventuele wijzigingen.',
-	'WARNING'	=> 'Waarschuwing',
-	'TOO_LONG_DATE'		=> 'De datum weergave die je hebt ingegeven is te lang.',
-	'TOO_SHORT_DATE'	=> 'De datum weergave die je hebt ingegeven is te kort.',
-	'TOO_SMALL_REFRESH'	=> 'De waarde voor het vernieuwen van de pagina is te klein.',
-	'TOO_LARGE_REFRESH'	=> 'De waarde voor het vernieuwen van de pagina is te groot.',
-	'TOO_SMALL_MESSAGE_LIMIT'	=> 'De waarde van de berichten limiet is te klein.',
-	'TOO_LARGE_MESSAGE_LIMIT'	=> 'De waarde van de berichten limiet is te groot.',
-	'TOO_SMALL_ARCHIVE_LIMIT'	=> 'De waarde van het archief limiet is te klein.',
-	'TOO_LARGE_ARCHIVE_LIMIT'	=> 'De waarde van het archief limiet is te groot.',
-	'TOO_SMALL_FLOOD_TIME'	=> 'De waarde van de hoeveelheid aan data in een bepaalde tijd is te klein.',
-	'TOO_LARGE_FLOOD_TIME'	=> 'De waarde van de hoeveelheid aan data in een bepaalde tijd is te groot.',
-	'TOO_SMALL_MAX_MESSAGE_LNGTH'	=> 'De waarde van de maximale lengte van berichten is te klein.',
-	'TOO_LARGE_MAX_MESSAGE_LNGTH'	=> 'De waarde van de maximale lengte van berichten is te groot.',
-	'TOO_SMALL_MAX_WORDS_LNGTH'	=> 'De waarde van de maximale lengte van het aantal woorden is te klein.',
-	'TOO_LARGE_MAX_WORDS_LNGTH'	=> 'De waarde van de maximale lengte van het aantal woorden is te groot.',
-	'TOO_SMALL_WHOIS_REFRESH'	=> 'De verversing van de whois waarde is te klein.',
-	'TOO_LARGE_WHOIS_REFRESH'	=> 'De verversing van de whois waarde is te groot.',
-	'TOO_SMALL_INDEX_HEIGHT'	=> 'De waarde van de index hoogte is te klein.',
-	'TOO_LARGE_INDEX_HEIGHT'	=> 'De waarde van de index hoogte is te groot.',
-	'TOO_SMALL_CUSTOM_HEIGHT'	=> 'De waarde van de gemaakte hoogte is te klein.',
-	'TOO_LARGE_CUSTOM_HEIGHT'	=> 'De waarde van de gemaakte hoogte is te groot.',
-	'TOO_SHORT_STATIC_MESSAGE'	=> 'De waarde van de statische berichten is te kort.',
-	'TOO_LONG_STATIC_MESSAGE'	=> 'De waarde van de statische berichten is te lang.',
-	'TOO_SMALL_TIMEOUT'	=> 'De waarde van de timeout voor gebruikers is te klein.',
-	'TOO_LARGE_TIMEOUT'	=> 'De waarde van de timeout voor gebruikers is te groot.',
-		// User perms
+	'MCHAT_WHOIS_REFRESH'					=> 'Wie is aan het chatten herlaad interval',
+	'MCHAT_WHOIS_REFRESH_EXPLAIN'			=> 'Aantal seconden voordat wie is aan het chatten herlaad.<br /><em>Er is een limiet van 30 tot 300 seconds.</em>',
+	'MCHAT_MESSAGE_TOP'						=> 'Locatie van nieuwe berichten',
+	'MCHAT_MESSAGE_TOP_EXPLAIN'				=> 'Nieuwe berichten worden getoond boven of onder in de chat.',
+	'MCHAT_BOTTOM'							=> 'Onder',
+	'MCHAT_TOP'								=> 'Boven',
+	'MCHAT_PAUSE_ON_INPUT'					=> 'Pauzeer bij input',
+	'MCHAT_PAUSE_ON_INPUT_EXPLAIN'			=> 'Herlaad de chat niet als de gebruiker een bericht schrijft',
+	'MCHAT_PURGE'							=> 'Verwijder nu alle berichten',
+	'MCHAT_PURGE_CONFIRM'					=> 'Bevestig het verwijderen van alle berichten',
+	'MCHAT_PURGED'							=> 'Alle mini-chat berichten zijn succesvol verwijderd',
+
+	// Error reporting
+	'TOO_SMALL_MCHAT_ARCHIVE_LIMIT'			=> 'De achrief limiet waarde is te klein.',
+	'TOO_LARGE_MCHAT_ARCHIVE_LIMIT'			=> 'De archief limiet waarde is te groot.',
+	'TOO_LONG_MCHAT_BBCODE_DISALLOWED'		=> 'De geweigerde bbcodes waarde is te lang.',
+	'TOO_SMALL_MCHAT_CUSTOM_HEIGHT'			=> 'De aangepaste hoogte waarde is te klein.',
+	'TOO_LARGE_MCHAT_CUSTOM_HEIGHT'			=> 'De aangepaste hoogte waarde is te groot.',
+	'TOO_LONG_MCHAT_DATE'					=> 'Het datumformaat dat je hebt ingevuld is te lang.',
+	'TOO_SHORT_MCHAT_DATE'					=> 'Het datumformaat dat je hebt ingevuld is te kort.',
+	'TOO_SMALL_MCHAT_FLOOD_TIME'			=> 'De opruim tijd waarde is te klein.',
+	'TOO_LARGE_MCHAT_FLOOD_TIME'			=> 'De opruim tijd waarde is te groot.',
+	'TOO_SMALL_MCHAT_INDEX_HEIGHT'			=> 'De index hoogte waarde is te klein.',
+	'TOO_LARGE_MCHAT_INDEX_HEIGHT'			=> 'De index hoogte waarde is te groot.',
+	'TOO_SMALL_MCHAT_MAX_MESSAGE_LNGTH'		=> 'De max berichten lengte waarde is te klein.',
+	'TOO_LARGE_MCHAT_MAX_MESSAGE_LNGTH'		=> 'De max berichten lengte waarde is te groot.',
+	'TOO_SMALL_MCHAT_MESSAGE_LIMIT'			=> 'De bericht limiet waarde is te klein.',
+	'TOO_LARGE_MCHAT_MESSAGE_LIMIT'			=> 'De bericht limiet waarde is te groot.',
+	'TOO_SMALL_MCHAT_MESSAGE_NUM'			=> 'Het aantal weer te geven berichten op de indexpagina is te klein.',
+	'TOO_LARGE_MCHAT_MESSAGE_NUM'			=> 'Het aantla weer te geven berichten op de indexpagina is te groot.',
+	'TOO_SMALL_MCHAT_REFRESH'				=> 'De herlaad waarde is te klein.',
+	'TOO_LARGE_MCHAT_REFRESH'				=> 'De herlaad waarde is te groot.',
+	'TOO_LONG_MCHAT_STATIC_MESSAGE'			=> 'De statisch bericht waarde is te lang.',
+	'TOO_SMALL_MCHAT_TIMEOUT'				=> 'De gebruikerstime-out waarde is te kort',
+	'TOO_LARGE_MCHAT_TIMEOUT'				=> 'De gebruikerstime-out waarde is te lang.',
+	'TOO_SMALL_MCHAT_WHOIS_REFRESH'			=> 'De whois herlaad waarde is te klein.',
+	'TOO_LARGE_MCHAT_WHOIS_REFRESH'			=> 'De whois herlaad waarde is te groot.',
+
+	// User perms
 	'ACL_U_MCHAT_USE'			=> 'Je kunt mChat gebruiken',
 	'ACL_U_MCHAT_VIEW'			=> 'Je kunt mChat bekijken',
 	'ACL_U_MCHAT_EDIT'			=> 'Je kunt mchat berichten bewerken',
-	'ACL_U_MCHAT_DELETE'		=> 'Je kunt mChat berichten verwijderen',
+	'ACL_U_MCHAT_DELETE'			=> 'Je kunt mChat berichten verwijderen',
 	'ACL_U_MCHAT_IP'			=> 'Je kunt mChat IP adressen bekijken',
-	'ACL_U_MCHAT_FLOOD_IGNORE'	=> 'Je kunt mChat de hoeveelheid aan data negeren',
-	'ACL_U_MCHAT_ARCHIVE'		=> 'Je kunt het archief van mChat bekijken',
-	'ACL_U_MCHAT_BBCODE'		=> 'Je kunt de bbcode in mChat gebruiken',
-	'ACL_U_MCHAT_SMILIES'		=> 'Je kunt de smilies in mChat gebruiken',
+	'ACL_U_MCHAT_PM'			=> 'Kan gebruik maken van privéberichten',
+	'ACL_U_MCHAT_LIKE'			=> 'Kan berichten leuk vinden',
+	'ACL_U_MCHAT_QUOTE'			=> 'Kan berichten quoten',
+	'ACL_U_MCHAT_FLOOD_IGNORE'		=> 'Je kunt mChat de hoeveelheid aan data negeren',
+	'ACL_U_MCHAT_ARCHIVE'			=> 'Je kunt het archief van mChat bekijken',
+	'ACL_U_MCHAT_BBCODE'			=> 'Je kunt de bbcode in mChat gebruiken',
+	'ACL_U_MCHAT_SMILIES'			=> 'Je kunt de smilies in mChat gebruiken',
 	'ACL_U_MCHAT_URLS'			=> 'Je kunt urls posten in mChat',
+	
 	// Admin perms
 	'ACL_A_MCHAT'				=> array('lang' => 'Kan mChat instellingen beheren', 'cat' => 'permissions'), // Using a phpBB category here
 ));
